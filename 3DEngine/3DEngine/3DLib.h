5268305aa8328a2581933542d9e7c3cc916d4428
mrschonb <63616199+mrschonb@users.noreply.github.com>
2020-06-11 16:53:05 -0500

    Add files via upload

--- 3DEngine/3DEngine/3DLib.h
+++ 3DEngine/3DEngine/3DLib.h
@@ -87,6 +87,39 @@ enum PIXEL_TYPE
 	PIXEL_QUARTER = 0x2591,
 };
 
+CHAR_INFO GetColour(float lum)
+{
+	short bg_col, fg_col;
+	wchar_t sym;
+	int pixel_bw = (int)(13.0f*lum);
+	switch (pixel_bw)
+	{
+	case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
+
+	case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
+	case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
+	case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
+	case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
+
+	case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
+	case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
+	case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
+	case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
+
+	case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
+	case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
+	case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
+	case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
+	default:
+		bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
+	}
+
+	CHAR_INFO c;
+	c.Attributes = bg_col | fg_col;
+	c.Char.UnicodeChar = sym;
+	return c;
+}
+
 class olcConsoleGameEngine
 {
 public:
