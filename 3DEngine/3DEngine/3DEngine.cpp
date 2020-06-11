c9ee8cdfd5a3bbcfaae0addf6fac683680134177
mrschonb <63616199+mrschonb@users.noreply.github.com>
2020-06-11 16:53:42 -0500

    Add files via upload

--- 3DEngine/3DEngine/3DEngine.cpp
+++ 3DEngine/3DEngine/3DEngine.cpp
@@ -43,39 +43,6 @@ public:
 		Theta = 0;
 	}
 
-	CHAR_INFO GetColour(float lum)
-	{
-		short bg_col, fg_col;
-		wchar_t sym;
-		int pixel_bw = (int)(13.0f*lum);
-		switch (pixel_bw)
-		{
-		case 0: bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID; break;
-
-		case 1: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_QUARTER; break;
-		case 2: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_HALF; break;
-		case 3: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_THREEQUARTERS; break;
-		case 4: bg_col = BG_BLACK; fg_col = FG_DARK_GREY; sym = PIXEL_SOLID; break;
-
-		case 5: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_QUARTER; break;
-		case 6: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_HALF; break;
-		case 7: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_THREEQUARTERS; break;
-		case 8: bg_col = BG_DARK_GREY; fg_col = FG_GREY; sym = PIXEL_SOLID; break;
-
-		case 9:  bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_QUARTER; break;
-		case 10: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_HALF; break;
-		case 11: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_THREEQUARTERS; break;
-		case 12: bg_col = BG_GREY; fg_col = FG_WHITE; sym = PIXEL_SOLID; break;
-		default:
-			bg_col = BG_BLACK; fg_col = FG_BLACK; sym = PIXEL_SOLID;
-		}
-
-		CHAR_INFO c;
-		c.Attributes = bg_col | fg_col;
-		c.Char.UnicodeChar = sym;
-		return c;
-	}
-
 public:
 	bool OnUserCreate() override {
 		//Load ObjFile
@@ -205,9 +172,9 @@ public:
 int main()
 {
 
-	olcEngine3D demo;
-	if (demo.ConstructConsole(256, 240, 4, 4))
-		demo.Start();
+	olcEngine3D console;
+	if (console.ConstructConsole(256, 240, 4, 4))
+		console.Start();
 	else {
 		perror("Error: ConstructConsole");
 	}
